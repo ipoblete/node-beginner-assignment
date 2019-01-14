8be8f1ed9d6064a8dc1ca12e5c103f1e519648de
Ivan <i.poblete915@gmail.com>
2019-01-13 18:35:33 -0800

    able to choose a file and upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,20 @@
-var exec = require('child_process').exec;
+var querystring = require('querystring'),
+  fs = require('fs'),
+  formidable = require('formidable');
 
-function start(response) {
+function start(response, postData) {
   console.log('Request handler start was called.');
 
   var body = '<html'+
     '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html;'+
-    'charset=UTF-8" />'+
+    '<meta http-equiv="Content-Type" '+ 
+    'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+ 
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -21,12 +24,33 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, request) {
   console.log('Request handled upload was called.');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello Upload');
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, 'tmp/logo.png', function(error){
+      if(error) {
+        fs.unlink('/tmp/logo.png');
+        fs.rename(files.upload.path, 'tmp/logo.png');
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
+}
+
+function show(response) {
+  console.log('Request handler show was called');
+  response.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('./tmp/logo.png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
